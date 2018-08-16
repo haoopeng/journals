49dcb3a60f37907c99f8b6e4a422f2d9f30ea05e
Hao Peng <penghao@umail.iu.edu>
2018-08-16 11:50:58 -0400

    add map

---
+++ colorcode.js
@@ -0,0 +1,16 @@
+var color2class = {
+  "#20c157": "bio",
+  "#0001fa": "chem",
+  "#6B8E23": "biotech",
+  "#8B0000": "diseases",
+  "#FF0000": "medical",
+  "#8B6508": "health",
+  "#CDC673": "brain",
+  "#ffff0c": "social",
+  "#EE9A00": "humanities",
+  "#fb01ff": "eecs",
+  "#663788": "mathphysics",
+  "#2dffff": "engineering",
+  "#FF6600": "earth",
+  "#000000": "interdiscipline"
+};
